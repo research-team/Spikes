a5b2bebad6eef9b1454238328ab1363764292863
Emil Melnikov <emilmelnikov@gmail.com>
2015-02-12 17:28:07 +0300

    Add PRB generation script and a correct generated PRB file

---
+++ klustakwik-input/prbgen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+"""Generate the probe file.
+"""
+
+
+from __future__ import print_function
+
+
+from itertools import combinations
+from pprint import PrettyPrinter
+
+
+PRB_FILE_NAME = 'experiment.prb'
+CHANNEL_GROUPS_COUNT = 4
+CHANNELS_IN_GROUP = 4
+
+
+def main():
+    channel_groups = {}
+    for i in range(CHANNEL_GROUPS_COUNT):
+        start = CHANNELS_IN_GROUP * i
+        end = start + CHANNELS_IN_GROUP
+        channels = list(range(start, end))
+        channel_groups[i] = {
+            'channels': channels,
+            'graph': list(combinations(channels, 2)),
+        }
+    with open(prb_file_name, 'w') as f:
+        f.write('channel_groups = \\\n')
+        PrettyPrinter(stream=f).pprint(channel_groups)
+
+
+if __name__ == '__main__':
+    main()
